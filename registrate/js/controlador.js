3003e4fe129f6c1929fcaf198aa9cd1763c0e5ee
joseEnrique9797 <48044322+joseEnrique9797@users.noreply.github.com>
2019-08-10 00:10:03 -0600

    Add files via upload

---
+++ registrate/js/controlador.js
@@ -0,0 +1,34 @@
+var localStorage = window.localStorage;
+var informacion
+$("#button-config").click(function(){
+    console.log("entro-1")
+
+    var datos={
+        nombre: $('#nombre').val(),
+        apellido:$('#apellidos').val(),
+        correo:$('#correo').val(),
+        contraseña:$('#contraseña').val()
+    }
+    console.log(datos);
+    
+    localStorage.setItem($('#nombre').val(), JSON.stringify(datos));
+    informacion=datos;
+
+});
+
+/*
+$(document).ready(function(){
+    
+        var d= JSON.parse(localStorage.getItem("qewrqwerqer"));
+        
+        $("#nombre-usuario").append(`
+            HOLA
+            ${d.apellido}
+                  
+        `); 
+    
+    
+});
+
+*/
+
